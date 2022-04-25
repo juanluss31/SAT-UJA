ad58b1aa5d08306575224bfe9535444793ebdeac
juanluss31 <jlherrerosbodalo@gmail.com>
2022-04-25 20:12:44 +0200

    25/04/2022

---
+++ Prácticas/3/app.js
@@ -0,0 +1,334 @@
+/* ********************************************************************************************
+ * Práctica 3. Aplicación web en el servidor
+ * Descripción: servidor Node.JS que implementa algunos servicios empleados en la práctica 2
+ * ASIGNATURA: Servicios y Aplicaciones Telemáticas
+ * TITULACIÓN: Grado en Ingeniería de tecnologías de telecomunicación (14312020)
+ * TITULACIÓN: Doble Grado Ing. de tecnologías de la telecomunicación e Ing. telemática (15212007)
+ * TITULACIÓN: Grado en Ingeniería telemática (14512016)
+ * CENTRO: ESCUELA POLITÉCNICA SUPERIOR (LINARES)
+ * CURSO ACADÉMICO: 2021-2022
+ * AUTOR/ES: Juan Luis Herreros Bódalo y Manuel Ballesteros Bosques
+ *********************************************************************************************/
+
+const express = require("express"); //Módulo de express
+const path = require("path"); //Módulo para manejar rutas de archivos
+const MongoClient = require("mongodb").MongoClient; // Módulo de gestión MongoDB
+const ObjectID = require("mongodb").ObjectID; // Modulo para poder emplear los objetos ID para referenciar documentos mongo
+const Mustache = require("mustache"); //Módulo para el motor de plantilla Mustache
+const mustacheExpress = require("mustache-express"); //Módulo para el motor de plantilla Mustache
+const dns = require("dns"); //Módulo para emplear el servicio DNS
+const os = require("os"); //Módulo de información relativa al sistema operativo y el host
+const { response } = require("express");
+
+const app = express(); //Instancia de Express
+
+const urlMongoDB = "mongodb://localhost:27017/examenes"; //URL de la base de datos MongoDB
+
+const DEFAULT_PORT = 8083; //Puerto del servidor por defecto
+const PORT = DEFAULT_PORT; //Para poder actualizarla por los argumentos del programa.
+
+//Mustache
+app.engine('html', mustacheExpress());
+app.set("view engine", "html");
+app.set("views", "./views");
+
+//Ruta a los recursos estáticos, normalmente CSS o html sin personalizar
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.json()); //Preparar req.body para contenido JSON
+app.use(express.urlencoded({ extended: true })); // Preparar req.body para contenido application/x-www-form-urlencoded
+
+/************************ 
+  Rutas a los servicios 
+*************************/
+
+/** [Aportado por el profesor] Simplemente muestra la petición de entrada como registro y pasa el manejo al siguiente  */
+app.use((req, res, next) => {
+  console.log("[SERVIDOR] Petición entrante:" + req.method + " " + req.path);
+  next(); //Cede el control a la siguiente parte de código que se ajuste con la petición.
+});
+
+/** [Aportado por el profesor] Servicio para crear usuario */
+app.put("/user", function (req, res) {
+  console.log("[SERVIDOR] PUT /user Recibido:" + JSON.stringify(req.body));
+
+  MongoClient.connect(
+    urlMongoDB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, db) {
+      if (err) {
+        res.status(500);
+        res.end();
+      } else {
+        const dbo = db.db("examenes");
+        //Se busca primero si el usuario ya existe.
+        dbo
+          .collection("users")
+          .countDocuments(
+            { user: { $eq: req.body.user } },
+            function (err, number) {
+              console.log(
+                number + " coincidencia/s del usuario:" + req.body.user
+              );
+              if (err) {
+                //Error de búsqueda de usuarios existentes
+                console.log("Error: " + err);
+                res.status(500);
+                res.end("{}");
+              } else if (number == 0) {
+                //Si el usuario no existe, se inserta
+                dbo
+                  .collection("users")
+                  .insertOne(req.body, function (err, result) {
+                    if (err) {
+                      //Error de inserción
+                      console.log("Error: " + err);
+                      res.status(500);
+                      res.end("{}");
+                    } else {
+                      //Usuario insertado correctamente
+                      console.log(
+                        "1 document inserted: " + JSON.stringify(result)
+                      );
+                      db.close();
+                      res.status(201);
+                      res.type("application/json");
+                      res.end(JSON.stringify(result));
+                    }
+                  });
+              } else {
+                //Usuario repetido
+                console.log("Usuario ya existe");
+                res.status(400);
+                res.end("{}");
+              }
+            }
+          );
+      }
+    }
+  );
+});
+
+// [Aportado por el profesor] Servicio para recibir los repositorios
+/* Este servicio es una emulación, solamente devuelve una constante */
+app.get("/repository/:user", function (req, res) {
+  console.log(`[SERVIDOR][/repository:${req.params.user}]`);
+  let json = [
+    {
+      _id: "6013d4393070e93b9d93dc32",
+      user: req.params.user,
+      name: "Matemáticas",
+    },
+    {
+      _id: "6013d4413070e93b9d93dc33",
+      user: req.params.user,
+      name: "Geografía",
+    },
+  ];
+
+  res.json(json);
+});
+
+/* [Aportado por el profesor] Servicio de login */
+app.post("/login", (req, res) => {
+  console.log("[SERVIDOR][/login] Recibido: " + JSON.stringify(req.body));
+
+  MongoClient.connect(
+    urlMongoDB,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    function (err, client) {
+      if (err) {
+        console.log("Error: " + err);
+        res.status(500).end();
+      } else {
+        var db = client.db("examenes");
+        db.collection("users")
+          .find({ user: req.body.user })
+          .toArray(function (err, result) {
+            if (err) {
+              res.status(500).end();
+            } else {
+              if (result != undefined && result !== null && result.length > 0) {
+                console.log(
+                  "[SERVIDOR][/login] Encontrado:" + JSON.stringify(result)
+                );
+                if (req.body.password === result[0].password) {
+                  res.status(200).end();
+                } else {
+                  res.status(401).end();
+                }
+              } else {
+                res.status(401).end();
+              }
+            }
+            client.close();
+          });
+      }
+    }
+  );
+});
+
+/* [Aportado por el profesor] Servicio GET exam para obtener la lista de todos los exámenes
+para que en la sección de nuevo examen se pueda mostar la lista de todos ellos y añadir
+el botón para borrar */
+app.get("/exam", function (req, res) {
+  console.log("[SERVIDOR][/exam]");
+
+  MongoClient.connect(
+    urlMongoDB,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    function (err, client) {
+      if (err) {
+        res.status(500).end();
+      } else {
+        var db = client.db("examenes");
+        db.collection("exams")
+          .find({})
+          .toArray(function (err, result) {
+            if (err) {
+              res.status(500).end();
+            } else {
+              res.json(result);
+            }
+            client.close();
+          });
+      }
+    }
+  );
+});
+
+/******* INICIO PRÁCTICA 3 **********/
+
+//Estos son los servicios que se deben crear en la práctica 3
+
+//Tarea 3: servicio PUT /exam
+app.put("/exam", (request, response) => {
+  if (checkExam(request.body)) {
+    MongoClient.connect(
+      urlMongoDB,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      function (err, client) {
+        if (err) {
+          response.status(500).end();
+        }
+        var db = client.db("examenes");
+        db.collection("exams").insertOne(request.body, function (err, result) {
+          if (err) {
+            response.status(500).end();
+          } else {
+            console.log("1 document inserted: " + JSON.stringify(result));
+            response.status(201);
+            response.type("application/json");
+            response.end(JSON.stringify(result));
+          }
+          client.close();
+        });
+      }
+    );
+  } else {
+    console.log("El formato recibido es incorrecto");
+    response.status(400).end();
+  }
+});
+
+//Tarea 4: servicio DELETE /exam/id
+app.delete("/exam/:id", (request, response) => {
+  MongoClient.connect(
+    urlMongoDB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, client) {
+      if (err) {
+        response.status(500).end();
+      } else {
+        var db = client.db("examenes");
+        try {
+          var postId = new ObjectID(request.params.id);
+          db.collection("exams").deleteOne(
+            { _id: postId },
+            function (err, result) {
+              if (err) {
+                response.status(500).end();
+              } else {
+                if (result.deletedCount > 0) {
+                  console.log("Deleted result: " + JSON.stringify(result));
+                  response.status(200).end();
+                } else {
+                  console.log("404 - No se ha encontrado en la base de datos");
+                  response.status(404).end();
+                }
+              }
+              client.close();
+            }
+          );
+        } catch (ex) {
+          console.log("Exception: " + ex);
+          response.status(404).end();
+        }
+      }
+    }
+  );
+});
+
+//Tarea 5: servicio GET /exam/user
+app.get("/exam/:user", (req, res) => {
+  res.status(200).end();
+});
+
+/******* FIN PRÁCTICA 3 **********/
+
+//Se busca la ip del host para mostrarla en el mensaje de inicio
+dns.lookup(os.hostname(), 4, function (err, address, family) {
+  //4 para IPv4
+  if (err) {
+    console.log("Error al obtener la IP del servidor");
+  } else {
+    console.log("IP del servidor: " + address.toString());
+    //Se inicia el servidor una vez se ha buscado la ip
+    app.listen(PORT, address.toString(), () => {
+      console.log(`Servidor ejecutándose en http://${address}:${PORT}`);
+    });
+  }
+});
+
+function checkExam(obj) {
+  let examen = {
+    user: "",
+    title: "",
+    date: "",
+    repository: "",
+  };
+
+  for (let prop in obj) if (examen[prop] == undefined) return false;
+  for (let prop in examen) if (obj[prop] == undefined) return false;
+
+  return true;
+}
+
+//Prueba:
+
+var user = {
+  user: "Joe Doe",
+  title:"Mr."
+  };
+
+app.get("/mustache", function (req, res) {
+  res.render("mustache", user, function (err, html) {
+    //En caso de error
+    if (err) {
+      res.write("Error: " + err + " " + html);
+    } else {
+      res.write(html);
+    }
+    res.end();
+  });
+});
+app.listen(3000);
---
+++ Prácticas/3/public/script.js
@@ -0,0 +1,261 @@
+/*
+
+PRÁCTICA 2: APLICACIÓN WEB CLIENTE BÁSICA
+
+SERVICIOS Y APLICACIONES TELEMÁTICAS 
+INGENIERÍA DE LAS TECNOLOGÍAS DE LA TELECOMUNICACIÓN
+
+CURSO 2021/22
+
+JUAN LUIS HERREROS BÓDALO - MANUEL BALLESTEROS BOSQUES
+
+*/
+
+var SERVERURL =  "";
+
+var currentUser = "";
+
+function show(n) {
+  let sections = document.querySelectorAll("[id^='section']");
+  let menus = document.querySelectorAll("[id^='menu']");
+  for (let s of sections) {
+    s.classList.add("oculto");
+  }
+  for (let m of menus) {
+    m.classList.remove("activo");
+  }
+
+  let oculto = document.getElementById("section-" + n);
+  oculto.classList.remove("oculto");
+  if (n != 4 && n != 5) {
+    let ocultoa = document.getElementById("section-" + n + "a");
+    ocultoa.classList.remove("oculto");
+  }
+
+  if (n == 5) loadExams();
+
+  let activo = document.getElementById("menu-" + n);
+  activo.classList.add("activo");
+}
+function menuLogeado(user) {
+  currentUser = user;
+  loadRepository();
+  loadExams();
+
+  let menus = document.querySelectorAll("[id^='menu']");
+
+  for (let m of menus) {
+    m.classList.add("oculto");
+  }
+  let nuevoExamen = document.getElementById("menu-3");
+  nuevoExamen.classList.remove("oculto");
+
+  let acercade = document.getElementById("menu-4");
+  acercade.classList.remove("oculto");
+
+  let listaExamenes = document.getElementById("menu-5");
+  listaExamenes.classList.remove("oculto");
+
+  show(3);
+}
+
+function login(event) {
+  event.preventDefault();
+
+  let user = document.forms.acceso.user.value;
+  let password = document.forms.acceso.passwordLogin.value;
+  let json = {
+    user: user,
+    password: password,
+  };
+  let conn = new XMLHttpRequest();
+  conn.open("POST", SERVERURL + "/login");
+  conn.onreadystatechange = function () {
+    if (conn.readyState === 4) {
+      switch (conn.status) {
+        case 200:
+          menuLogeado(user);
+          break;
+        case 401:
+          escribirError(1, "Error de autenticación");
+          break;
+        case 500:
+          escribirError(1, "Error de autenticación");
+          break;
+      }
+    }
+  };
+  conn.setRequestHeader("Content-Type", "application/json");
+  conn.send(JSON.stringify(json));
+}
+
+function user(event) {
+  event.preventDefault();
+
+  let user = document.forms.register.nick.value;
+  let password = document.forms.register.password.value;
+  let name = document.forms.register.name.value;
+  let surname = document.forms.register.surname.value;
+  let email = document.forms.register.email.value;
+  let json = {
+    "user": user,
+    "password": password,
+    "name": name,
+    "surname": surname,
+    "email": email,
+  };
+  console.log(JSON.stringify(json));
+  let conn = new XMLHttpRequest();
+  conn.open("PUT", SERVERURL + "/user");
+  conn.onreadystatechange = function () {
+    if (conn.readyState === 4) {
+      switch (conn.status) {
+        case 201:
+          //Usuario logueado
+          menuLogeado(user);
+          break;
+        case 400:
+          escribirError(2, "Ya existe un usuario igual en el sistema");
+          break;
+        case 500:
+          escribirError(2, "Error del servidor");
+          break;
+      }
+    }
+  };
+  conn.setRequestHeader("Content-Type", "application/json");
+  conn.send(JSON.stringify(json));
+}
+
+function loadRepository() {
+  let conn = new XMLHttpRequest();
+  conn.open("GET", SERVERURL + "/repository/" + currentUser);
+  conn.onload = function () {
+    if (conn.readyState === 4) {
+      switch (conn.status) {
+        case 200:
+          //Se recibe el JSON
+          let repositorios = JSON.parse(conn.responseText);
+          listarRepositorios(repositorios);
+          break;
+        case 403:
+          console.log("Acceso prohibido");
+          break;
+      }
+    }
+  };
+  conn.send(null);
+}
+
+function listarRepositorios(repo) {
+  let lista = document.getElementById("repositorioSelector");
+
+  for (let item of repo) {
+    let opcion = document.createElement("option");
+    opcion.setAttribute("value", item._id);
+    opcion.innerHTML = item.name;
+    lista.appendChild(opcion);
+  }
+}
+
+function crearExamen(event) {
+  event.preventDefault();
+
+  let user = currentUser;
+  let title = document.forms.exam.title.value;
+  let date = document.forms.exam.date.value;
+  let repository = document.forms.exam.repository.value;
+
+  let json = {
+    "user": user,
+    "title": title,
+    "date": date,
+    "repository": repository,
+  };
+  console.log(JSON.stringify(json));
+  let conn = new XMLHttpRequest();
+  conn.open("PUT", SERVERURL + "/exam");
+  conn.onreadystatechange = function () {
+    if (conn.readyState === 4) {
+      switch (conn.status) {
+        case 201:
+          escribirError(3, "Examen creado");
+          break;
+        case 500:
+          escribirError(3, "Error del servidor");
+          break;
+      }
+    }
+  };
+  conn.setRequestHeader("Content-Type", "application/json");
+  conn.send(JSON.stringify(json));
+}
+
+function escribirError(n, c) {
+  let aside = window.document.getElementById("section-" + n + "a");
+  let parrafo = window.document.getElementById("error");
+  if (parrafo === null) {
+    parrafo = document.createElement("p");
+    parrafo.setAttribute("id", "error");
+  }
+  parrafo.innerHTML = c;
+  aside.appendChild(parrafo);
+}
+
+function loadExams() {
+  let conn = new XMLHttpRequest();
+  conn.open("GET", SERVERURL + "/exam");
+  conn.onload = function () {
+    if (conn.readyState === 4) {
+      switch (conn.status) {
+        case 200:
+          //Se recibe el JSON
+          let examenes = JSON.parse(conn.responseText);
+          listarExamenes(examenes);
+          break;
+        case 403:
+          console.log("Acceso prohibido");
+          break;
+      }
+    }
+  };
+  conn.send(null);
+}
+
+function listarExamenes(exam) {
+  let lista = document.getElementById("listExamen");
+  lista.innerHTML = "";
+
+  for (let item of exam) {
+    let p = document.createElement("p");
+    let b = document.createElement("button");
+    p.setAttribute("id", item._id);
+    p.innerHTML = item.title + " ";
+    b.setAttribute("id", item._id);
+    b.setAttribute("onclick", "eliminarExamen('" + item._id + "')");
+    b.innerHTML = "Borrar";
+    b.classList.add("boton");
+    p.appendChild(b);
+    lista.appendChild(p);
+  }
+}
+
+function eliminarExamen(id) {
+  let conn = new XMLHttpRequest();
+  conn.open("DELETE", SERVERURL + "/exam/" + id);
+  conn.onload = function () {
+    if (conn.readyState === 4) {
+      switch (conn.status) {
+        case 200:
+          let p = document.getElementById(id);
+          p.remove();
+          break;
+        case 403:
+          console.log("Acceso prohibido");
+          break;
+      }
+    }
+  };
+
+  conn.send(null);
+}
